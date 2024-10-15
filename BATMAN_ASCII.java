6af690ad432c204dbf0f5750902f76f904e1ff11
GIRIRAJ SANTOSH GURAV <127548343+giriraj-g0511@users.noreply.github.com>
2024-10-16 00:27:53 +0530

    Attached files

---
+++ BATMAN_ASCII.java
@@ -0,0 +1,31 @@
+
+public class BATMAN_ASCII {
+    public static void main(String[] args) {
+
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%SSS");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%SSSSSS");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%SS%%%%%%%%%%%%%%%%%%%%%%%%%SS%%SSSSSSSS");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%SSS@#S%%%%%%%%%%%%%%%%%SSSSSSSSSSSSSSSSSS");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%S%S%%SSS%SS#@@@@@@#S%SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%SSSSSSSSSSSSSSSSSSS@@@@@@@@@@#SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%SSSSSSSSSSSSSSSSSSS@@@@@@@@@@@@SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%SSSSSSSSSS#@@@@@@@@@@@#%%%SSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%SSSS%S@@@@@@@@@@@@SSS%%SSSSSSSSSSSSSSSSSSSSSSSSSS");
+		System.out.println("%%%%%%%SSSSSSSSSS%%%%%%%%%%%%%%%%%%%S%%%%SS%%%%#@@@@@@@@@@@@@@#SS%%SSSSSSSSSSSSSSSSSSSSSSS");
+		System.out.println("%SSSSSSSSSSSSSSSS%%%%%%SSSSSSSSSSSSSSS%SSSSSSSSSS#@@@@@@@@@@@@@@##SSSSSSSSSSSSSSSSSSSSSSSS");
+		System.out.println("SSSSSSSSSSSSSSSSSSS%SSSSSSSSSSSSSSSSSSSSSSSSSSSSSS###@@@@@@@@@@@@@@##SSSSSSSSSSSSSSSSSSSSS");
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS#@@@@@@@@@@@@@@@SSSSSSSSSSSSSSSSSSSS");
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS##@@@@@@@@@@@@@@@#SSSSSSSSSSSSSSSSSS");
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS@@#@@@@@@@@@@@@@@@@@SSSSSSSSSSSSSSSSS");
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS##@@@@@@@@@@@@@@@@@@@@@##SSSSSSSSSSSSSS");
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS#@@@@@@@@@@@@@@@@@@@@@@@@@@##SSSSSSSSSSS");
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@##SSSSSSSS");
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@##SSSSS");
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@##SS");
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    }
+}
